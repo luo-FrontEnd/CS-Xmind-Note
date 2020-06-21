a1d8a23c02500e1d243e9249846f5df11c2d4863
誓言 <oncwnuP7lx0Dc1syb0IW7J8RnJ9o@git.weixin.qq.com>
2020-06-21 22:12:36 +0800

    休息一天

--- 数据结构/1、线性表/leetcode.js
+++ 数据结构/1、线性表/leetcode.js
@@ -1,4 +1,4 @@
-//二维数组中的查找
+//二维数组中的查找--二维数组                                     
     /*
         在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
         来源：力扣（LeetCode）
@@ -23,7 +23,7 @@ var findNumberIn2DArray = function(matrix, target) {
     return false;
 }
 
-    //第二种方法
+    //第二种方法 单层循环，时间复杂度O(n+m)
 var findNumberIn2DArray2 = function(matrix, target) {
     let n = matrix.length; 
     if (!n) 
@@ -44,4 +44,30 @@ var findNumberIn2DArray2 = function(matrix, target) {
         }
     }
     return false;
+}
+
+//旋转矩阵--二维数组
+    /*
+        给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+        不占用额外内存空间能否做到？
+            给定 matrix = 
+            [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+            ],
+
+            原地旋转输入矩阵，使其变为:
+            [
+            [7,4,1],
+            [8,5,2],
+            [9,6,3]
+            ]
+
+            来源：力扣（LeetCode）
+            链接：https://leetcode-cn.com/problems/rotate-matrix-lcci
+            著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    */
+var rotate = function(matrix) {
+
 }
